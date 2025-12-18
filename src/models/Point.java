752993a77e6ddbca9e91d995643a256d55e2ad4f
Ansagan Murat <ansaganmurat29@gmail.com>
2025-12-18 23:42:50 +0500

    started assignment 1

---
+++ src/models/Point.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+    }
+
+    // constructor having both parameters
+
+    // distance(Point dest) - distance from current point to the dest point
+
+    // toString
+}
