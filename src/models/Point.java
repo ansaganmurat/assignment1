46c23575149b4ad05aa7f77bfc9e9d0031b0a51d
Ansagan Murat <ansaganmurat29@gmail.com>
2025-12-21 17:48:15 +0500

    started assignment 1

--- src/models/Point.java
+++ src/models/Point.java
@@ -4,19 +4,17 @@ public class Point {
     private double x;
     private double y;
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
+    // constructor having both parameters
+    public Point(double x, double y) {
         this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
         this.y = y;
     }
+
+    // distance from current point to the dest point
+    public double dis(Point dest) {
+        double x2 = dest.x - this.x;
+        double y2 = dest.y - this.y;
+        return Math.sqrt(x2 * x2 + y2 * y2);
+    }
+
 }
--- src/models/Shape.java
+++ src/models/Shape.java
@@ -1,13 +1,51 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Shape {
-    // container of Points (e.g. ArrayList<Point>)
 
-    // addPoint(Point) - adds to the container
+    // container of Points
+    private ArrayList<Point> points = new ArrayList<>();
 
-    // calculatePerimeter()
+    // adds point to container
+    public void addPoint(Point point) {
+        points.add(point);
+    }
 
-    // getAverageSide()
+    // calculates perimeter
+    public double calculatePerimeter() {
+        double perimeter = 0;
 
-    // getLongestSide()
+        for (int i = 0; i < points.size(); i++) {
+            Point curnt = points.get(i);
+            Point next = points.get((i + 1) % points.size());
+            perimeter += curnt.dis(next);
+        }
+
+        return perimeter;
+    }
+
+    // average side length
+    public double getAverageSide() {
+        if (points.size() == 0)
+            return 0;
+        return calculatePerimeter() / points.size();
+    }
+
+    // longest side
+    public double getLongest() {
+        double max = 0;
+
+        for (int i = 0; i < points.size(); i++) {
+            Point curnt = points.get(i);
+            Point next = points.get((i + 1) % points.size());
+
+            double dis = curnt.dis(next);
+            if (dis > max) {
+                max = dis;
+            }
+        }
+
+        return max;
+    }
 }
